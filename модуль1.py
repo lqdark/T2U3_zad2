13f6d8b6519847b2748adeca927403ecec8c373c
lqdark <lqdark@mail.ru>
2024-08-11 13:49:50 +0500

    Добавьте файлы проекта.

---
+++ модуль1.py
@@ -0,0 +1,15 @@
+print('ÒÅÑÒ' )
+st1=input()
+print('Enter the age of animal' )
+st2=input()
+print("Enter the animal's nickname" )
+st3=input()
+print('Òåñò' )
+st4=input()
+print('Enter the age of animal' )
+st5=input()
+print("Enter the animal's nickname" )
+st6=input()
+
+print()
+
